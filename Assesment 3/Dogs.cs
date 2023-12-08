20d4621144efe4f5acaaa150afd2216eafea62f5
ViKiN6 <5100164@mypillar7uwc.ac.za>
2023-12-08 14:33:07 +0200

    Final project

---
+++ Assesment 3/Dogs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assesment_3
+{
+    internal class Dogs
+    {
+
+          
+        private string WhatColour;
+        private int Legs;
+        private bool Gender;
+
+        public Dogs(string Whatcolour, int legs, bool gender) 
+        {
+            WhatColour = Whatcolour;
+            Legs = legs;    
+            Gender = gender;    
+        
+        }
+        public string GetColour() 
+        { 
+         return WhatColour;
+        }
+        public int GetLegs() 
+        {
+            return Legs;
+        }
+        public bool GetGender() 
+        {
+          return Gender;
+        }
+           
+        
+    }
+}
---
+++ Assesment 3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Assesment_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dogs MyDog = new Dogs("Brown", 4, true);
+
+            Console.WriteLine("What Colour is the Dog? " + MyDog.GetColour());
+            Console.WriteLine("How many legs? " + MyDog.GetLegs());
+            if (MyDog.GetGender())
+            {
+                Console.WriteLine("Gender : Male Dog ");
+
+            }
+            else 
+            {
+
+                Console.WriteLine("Gender : Female Dog ");
+            }
+        }
+    }
+}
